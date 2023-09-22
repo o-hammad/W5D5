9996f77b00bf8838b10629a4e2d5fbfce2e3eb96
Omar <ohammad@gwu.edu>
2023-09-22 11:14:55 -0400

    working on phase 02 question 3

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,12 +1,17 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where("yr BETWEEN 1980 AND 1989 AND score BETWEEN 3 AND 5")
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+    .group(:yr)
+    .having("MAX(score) <= 8")
+    .pluck(:yr)
 end
 
 def cast_list(title)
